4a142818446d96764847e1fdcdfcb12e12f80582
Tal Piven <67857533+Charizard13@users.noreply.github.com>
2024-04-04 19:42:21 +0300

    Update utils.go

--- utils.go
+++ utils.go
@@ -15,7 +15,7 @@ func handleNewTnx(body io.Reader) error {
 		return err
 	}
 
-	req.Header.Set("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0")
+	req.Header.Set("Authorization", "Bearer XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
 	req.Header.Set("Content-Type", "application/json")
 
 	client := &http.Client{}
